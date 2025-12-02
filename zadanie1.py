3f67a7525822027700d888c6b80898cb4bdf3793
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-02 21:04:55 +0100

    zadanie 1: lista 5 imion

--- zadanie1.py
+++ zadanie1.py
@@ -1,10 +1,6 @@
-def wyswietl_liczby_parzyste(lista_liczb):
-    for i in range (len(lista_liczb)):
-        x = lista_liczb[i]
-        if x % 2 == 0:
-            print(x)
+def wyswietl_imiona(lista_imion):
+   for x in lista_imion:
+       print(x)
 
-
-moja_lista = [10, 15, 22, 33, 40, 55, 60]
-print("Parzyste elementy to:")
-wyswietl_liczby_parzyste(moja_lista)
+imiona = ["Hania", "Kasia", "Jarek", "Michał", "Tomek"]
+wyswietl_imiona(imiona)
