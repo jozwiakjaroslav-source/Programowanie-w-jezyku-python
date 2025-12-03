3d49123ee6ee44ce579ff76ea1e96bfd31e9582c
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-03 17:39:46 +0100

    Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb, każdy jej element pomnoży przez 2, a na końcu ją zwróci, wersja 1 petla for

---
+++ zadanie2.py
@@ -0,0 +1,19 @@
+"""
+Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb,
+ każdy jej element pomnoży przez 2, a na końcu ją zwróci
+"""
+
+#weraja pierwsza tj z uzyciem petli for
+
+def mnozymy_przez_dwa(lista_startowa):
+    lista_z_wynikami = []
+    for liczba in lista_startowa:
+        lista_z_wynikami.append(liczba * 2)
+    return lista_z_wynikami
+
+# --- Przykład użycia (Poprawiony) ---
+liczby = [1, 2, 3, 4, 5]
+wynik = mnozymy_przez_dwa(liczby)
+
+
+print(wynik)
