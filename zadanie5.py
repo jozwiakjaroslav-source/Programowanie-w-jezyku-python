04413904a430b486896becea448d62b36a9bbe13
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-12 17:54:27 +0100

    LISTA PRZEDSTAWIAJACA TYLKO CO DRUGI ELEMNT

--- zadanie5.py
+++ zadanie5.py
@@ -2,6 +2,7 @@
 """Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range),
  a następnie wyświetli co drugi element."""
 
+
 def wyswietl_co_drugi(lista):
     print("Wyświetlanie co drugiego elementu listy:")
     co_drugi = lista[::2]
