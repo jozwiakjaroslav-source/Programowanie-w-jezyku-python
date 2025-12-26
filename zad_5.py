498a85e60dc7e9713f968ed6e605494cb8e697b0
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 18:47:41 +0100

    Stworzyć funkcję, która przyjmie 2 argumenty. Pierwszy typu list, a drugi typu int . Funkcja ma sprawdzić (zwracając typ logiczny bool), czy lista z parametru pierwszego zawiera taką wartość jaką przekazano w parametrze drugim.

--- zad_5.py
+++ zad_5.py
@@ -1,6 +1,13 @@
 # Zadanie 5
+"""Stworzyć funkcję, która przyjmie 2 argumenty. Pierwszy typu list, a drugi typu int .
+Funkcja ma sprawdzić (zwracając typ logiczny bool),
+czy lista z parametru pierwszego zawiera taką wartość jaką przekazano w parametrze drugim.
+"""
+
+
 def czy_lista_zawiera(lista_elementow: list, szukana_wartosc: int) -> bool:
     return szukana_wartosc in lista_elementow
+
 
 lista_ocen = [10, 20, 35, 40, 50]
 szukana_liczba = 35
