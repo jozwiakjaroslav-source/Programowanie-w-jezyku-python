509c8bd70e9fab55d9db0e75f3202a2ac43594de
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 17:52:57 +0100

    Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range), a następnie wyświetli jedynie parzyste elementy.

--- zadanie4.py
+++ zadanie4.py
@@ -1,6 +1,6 @@
-
 """Utwórz funkcję, która otrzyma w parametrze listę 10 liczb (rekomendowane wykorzystanie funkcji range),
- a następnie wyświetli jedynie parzyste elementy."""
+a następnie wyświetli jedynie parzyste elementy."""
+
 
 def wyswietl_parzyste(lista_liczb):
     print("Elementy parzyste z listy:")
@@ -8,6 +8,6 @@ def wyswietl_parzyste(lista_liczb):
         if liczba % 2 == 0:
             print(liczba)
 
+
 lista_liczb = list(range(1, 11))
 wyswietl_parzyste(lista_liczb)
-
