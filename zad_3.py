86628208ae6ea57ef55a68305d0186ea2ae5271a
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 18:46:31 +0100

    Stworzyć funkcję, która będzie sprawdzała czy przekazana liczba w parametrze jest parzysta i zwróci tą informację jako typ logiczny

--- zad_3.py
+++ zad_3.py
@@ -1,6 +1,15 @@
 # Zadanie 3
+"""Stworzyć funkcję, która będzie sprawdzała czy przekazana liczba
+w parametrze jest parzysta i zwróci tą informację jako typ logiczny bool (True/False).
+Należy uruchomić funkcję, wynik wykonania zapisać do zmiennej,
+ a następnie wykorzystując warunek logiczny wyświetlić prawidłowy tekst
+  "Liczba parzysta" / "Liczba nieparzysta"""
+
+
 def czy_parzysta(liczba: int) -> bool:
     return liczba % 2 == 0
+
+
 liczba_do_sprawdzenia = 24
 jest_parzysta = czy_parzysta(liczba_do_sprawdzenia)
 
