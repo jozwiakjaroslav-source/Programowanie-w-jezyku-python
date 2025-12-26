45b4a7d24f161dfc5a8147425356ce99f230ac74
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 18:47:03 +0100

    Stworzyć funkcję, która przyjmie 3 argumenty typu int i sprawdzi czy suma dwóch pierwszych liczb jest większa lub równa trzeciej, a następnie zwróci tę informację jako typ logiczny bool

--- zad_4.py
+++ zad_4.py
@@ -1,6 +1,12 @@
 # Zadanie 4
+"""Stworzyć funkcję, która przyjmie 3 argumenty typu int i sprawdzi
+czy suma dwóch pierwszych liczb jest większa lub równa trzeciej,
+ a następnie zwróci tę informację jako typ logiczny bool"""
+
+
 def porownaj_sume(a: int, b: int, c: int) -> bool:
     return (a + b) >= c
+
 
 wynik_porownania = porownaj_sume(10, 5, 12)
 
