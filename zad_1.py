597abe0d6ee52340cd25bf9b032c733ee31d28dc
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 18:45:32 +0100

    Stworzyć funkcję, która przyjmuje 2 argumenty

--- zad_1.py
+++ zad_1.py
@@ -1,6 +1,13 @@
 # Zadanie 1
+"""Stworzyć funkcję, która przyjmuje 2 argumenty (typu string) name i surname,
+a następnie zwróci stringa zgodnie ze wzorem Cześć {name} {surname}!
+ Należy uruchomić funkcję, wynik wykonania funkcji zapisać do zmiennej, a następnie go wyświetlić (print)
+"""
+
+
 def stworz_powitanie(name: str, surname: str) -> str:
     return f"Cześć {name} {surname}!"
+
 
 wynik_powitania = stworz_powitanie("Tomek", "Tomkowski")
 
