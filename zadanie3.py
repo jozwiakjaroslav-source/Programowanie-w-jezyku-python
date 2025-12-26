b20303441cec1c43a1abf0a8aa028c9a49f1b13e
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 17:52:15 +0100

    Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb,  każdy jej element pomnoży przez 2, a na końcu ją zwróci

--- zadanie3.py
+++ zadanie3.py
@@ -1,8 +1,10 @@
 """Utwórz funkcję, która otrzyma w parametrze listę zawierającą 5 dowolnych liczb,
- każdy jej element pomnoży przez 2, a na końcu ją zwróci."""
+każdy jej element pomnoży przez 2, a na końcu ją zwróci."""
+
 
 def pomnoz_przez_dwa_skladana(liczby_podstawowe):
     return [liczba * 2 for liczba in liczby_podstawowe]
+
 
 liczby_podstawowe = [5, 10, 15, 20, 25]
 wynik = pomnoz_przez_dwa_skladana(liczby_podstawowe)
