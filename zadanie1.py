a8ae3fd8e6bf6e22012f2b4d3186bef65d44e34d
Jarek Jozwiak <jozwiak.jaroslav@gmail.com>
2025-12-26 17:46:17 +0100

    Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.

--- zadanie1.py
+++ zadanie1.py
@@ -1,8 +1,10 @@
 # zadanie 1: Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.
 
+
 def wyswietl_imiona(lista_imion):
-   for x in lista_imion:
-       print(x)
+    for x in lista_imion:
+        print(x)
+
 
 imiona = ["Hania", "Kasia", "Jarek", "Michał", "Tomek"]
 wyswietl_imiona(imiona)
